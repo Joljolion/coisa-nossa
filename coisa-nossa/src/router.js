191ddfc2f4ce4e7b82cc2919d241a3283b7342a9
Joljolion <joaolazarini10@gmail.com>
2024-10-11 14:41:56 -0300

    links do router

---
+++ coisa-nossa/src/router.js
@@ -0,0 +1,35 @@
+import App from "./App.vue";
+import AboutUsPage from "./components/pages/AboutUsPage.vue";
+import ContactPage from "./components/pages/ContactPage.vue";
+import HomePage from "./components/pages/HomePage.vue";
+import LoginPage from "./components/pages/LoginPage.vue";
+
+const routes = [
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginPage
+    },
+    {
+        path: '/contact',
+        name: 'Contato',
+        component: ContactPage
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: App
+    },
+    {
+        path: '/about',
+        name: 'Sobre nós',
+        component: AboutUsPage
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
